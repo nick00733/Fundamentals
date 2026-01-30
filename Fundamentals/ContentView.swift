7d3b1d6a516f4bcc34928d848664290b6ed727df
Nick Nedjat <nick.nedjat@icloud.com>
2026-01-30 17:23:48 -0500

    Initial Commit

---
+++ Fundamentals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fundamentals
+//
+//  Created by Nick Nedjat on 30.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Nick Nedjat on 30.01.26.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
